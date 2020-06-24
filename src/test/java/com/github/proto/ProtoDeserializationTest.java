5fe4346ce7e808c90caa1d5e2296078ea1a0da4a
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2020-06-24 11:23:35 +0300

    Reformat test cases to inline the test data

--- src/test/java/com/github/proto/ProtoDeserializationTest.java
+++ src/test/java/com/github/proto/ProtoDeserializationTest.java
@@ -12,55 +12,53 @@ final class ProtoDeserializationTest {
     @Test
     @DisplayName("not throw exceptions if all field names are in `lowerCamelCase` style")
     void deserializeWithLowerCamelCase() {
+        String camelCasePubsubJsonProto = "" +
+                "{\n" +
+                "  \"message\": {\n" +
+                "    \"data\": \"eyJrZXkiOiJ2YWx1ZSJ9\",\n" +
+                "    \"messageId\": \"450292511223766\",\n" +
+                "    \"publishTime\": \"2020-06-21T20:48:25.908Z\"\n" +
+                "  },\n" +
+                "  \"subscription\": \"projects/test-project/subscriptions/test-subscription\"\n" +
+                "}";
         Assertions.assertDoesNotThrow(() -> {
-            Json.fromJson(CAMEL_CASE_PUBSUB_JSON_PROTO, PubsubPushNotification.class);
+            Json.fromJson(camelCasePubsubJsonProto, PubsubPushNotification.class);
         });
     }
 
     @Test
     @DisplayName("not throw exceptions if all field names are in `snake_case` style")
     void deserializeWithSnakeCase() {
+        String snakeCasePubsubJsonProto = "" +
+                "{\n" +
+                "  \"message\": {\n" +
+                "    \"data\": \"eyJrZXkiOiJ2YWx1ZSJ9\",\n" +
+                "    \"message_id\": \"450292511223766\",\n" +
+                "    \"publish_time\": \"2020-06-21T20:48:25.908Z\"\n" +
+                "  },\n" +
+                "  \"subscription\": \"projects/test-project/subscriptions/test-subscription\"\n" +
+                "}";
         Assertions.assertDoesNotThrow(() -> {
-            Json.fromJson(SNAKE_CASE_PUBSUB_JSON_PROTO, PubsubPushNotification.class);
+            Json.fromJson(snakeCasePubsubJsonProto, PubsubPushNotification.class);
         });
     }
 
     @Test
     @DisplayName("throw exception if there is same field in `snake_case` and `lowerCamelCase`")
     void deserializeBothStyles() {
+        String bothStylesPubsubJsonProto = "" +
+                "{\n" +
+                "  \"message\": {\n" +
+                "    \"data\": \"eyJrZXkiOiJ2YWx1ZSJ9\",\n" +
+                "    \"messageId\": \"450292511223766\",\n" +
+                "    \"message_id\": \"450292511223766\",\n" +
+                "    \"publishTime\": \"2020-06-21T20:48:25.908Z\",\n" +
+                "    \"publish_time\": \"2020-06-21T20:48:25.908Z\"\n" +
+                "  },\n" +
+                "  \"subscription\": \"projects/test-project/subscriptions/test-subscription\"\n" +
+                "}";
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Json.fromJson(BOTH_STYLES_PUBSUB_JSON_PROTO, PubsubPushNotification.class);
+            Json.fromJson(bothStylesPubsubJsonProto, PubsubPushNotification.class);
         });
     }
-
-    private static final String BOTH_STYLES_PUBSUB_JSON_PROTO = "" +
-            "{\n" +
-            "  \"message\": {\n" +
-            "    \"data\": \"eyJrZXkiOiJ2YWx1ZSJ9\",\n" +
-            "    \"messageId\": \"450292511223766\",\n" +
-            "    \"message_id\": \"450292511223766\",\n" +
-            "    \"publishTime\": \"2020-06-21T20:48:25.908Z\",\n" +
-            "    \"publish_time\": \"2020-06-21T20:48:25.908Z\"\n" +
-            "  },\n" +
-            "  \"subscription\": \"projects/test-project/subscriptions/test-subscription\"\n" +
-            "}";
-    private static final String CAMEL_CASE_PUBSUB_JSON_PROTO = "" +
-            "{\n" +
-            "  \"message\": {\n" +
-            "    \"data\": \"eyJrZXkiOiJ2YWx1ZSJ9\",\n" +
-            "    \"messageId\": \"450292511223766\",\n" +
-            "    \"publishTime\": \"2020-06-21T20:48:25.908Z\"\n" +
-            "  },\n" +
-            "  \"subscription\": \"projects/test-project/subscriptions/test-subscription\"\n" +
-            "}";
-
-    private static final String SNAKE_CASE_PUBSUB_JSON_PROTO = "" +
-            "{\n" +
-            "  \"message\": {\n" +
-            "    \"data\": \"eyJrZXkiOiJ2YWx1ZSJ9\",\n" +
-            "    \"message_id\": \"450292511223766\",\n" +
-            "    \"publish_time\": \"2020-06-21T20:48:25.908Z\"\n" +
-            "  },\n" +
-            "  \"subscription\": \"projects/test-project/subscriptions/test-subscription\"\n" +
-            "}";
 }
